bcfc8fb73d2d4a0ab8362ca07807a0283e088c57
NochEnnael <55722322+NochEnnael@users.noreply.github.com>
2020-03-09 11:36:39 -0400

    JS file with loops

--- src/zodiacjava.js
+++ src/zodiacjava.js
@@ -49,15 +49,35 @@ const belle = document.getElementById('belle')
 const tangled = document.getElementById('tangled')
 const up = document.getElementById('up')
 
-
-  function changeText(para){
-    intro.style.display = 'none'; // hides the intro paragraph
-    para.style.display = 'block'; // shows the text that is hidden that mathches the images based on parameter 'para'
+const paras = document.getElementsByClassName('description');
+// if the button we click is not the one we want, the there is no paragrpah shown. If it is the one that we want/ clicked, then the paragraph will appear. 
+function changeText(para){
+  for (var i = 0; i < paras.length; i++) {
+    if(para != paras[i]){
+      paras[i].style.display = "none";
+    }
+    else if (para == paras[i]) {
+      paras[i].style.display = 'block';
+    }
   }
-
+  /* intro.style.display = 'none'; // hides the intro paragraph
+  para.style.display = 'block'; // shows the text that is hidden that mathches the images based on parameter 'para' */
+}
+const buttonopacity = document.getElementsByClassName('soundtrack');
+// If the button we click is not clicked on, then it has an opacity of 0.4. If it IS the butotn clicked on then the opacity is 1.  
+function changeOpacity(opaque) {
+  for (var i = 0; i < buttonopacity.length; i++) {
+    if(opaque != buttonopacity[i]){
+      buttonopacity[i].style.opacity = '0.4';
+    } else if (opaque == buttonopacity[i]) {
+      buttonopacity[i].style.opacity = '1';
+    }
+  }
+}
   ariel.addEventListener('click', function() {
     changeText(aquarius);
-    ariel.style.opacity = '1';
+    changeOpacity(ariel)
+    /* ariel.style.opacity = '1';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -68,9 +88,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
     
-    aquarius.style.display = 'block';
+   /*  aquarius.style.display = 'block';
     pisces.style.display = 'none';
     aries.style.display = 'none';
     taurus.style.display = 'none';
@@ -81,15 +101,15 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
 
     //Adding event listener to the ariel image. Once it's clicked, it calls a function inside, which is changeText.
   });
 
   incred.addEventListener('click', function(){
     changeText(pisces);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(incred)
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '1';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -100,9 +120,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    pisces.style.display = 'block';
+    /* pisces.style.display = 'block';
     aquarius.style.display = "none";
     aries.style.display = 'none';
     taurus.style.display = 'none';
@@ -113,13 +133,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   mike.addEventListener('click', function(){
     changeText(aries);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(mike)
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '1';
     apple.style.opacity = '0.4';
@@ -130,9 +150,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    aries.style.display = 'block';
+    /* aries.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     taurus.style.display = 'none';
@@ -143,13 +163,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   apple.addEventListener('click', function() {
     changeText(taurus);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(apple);
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '1';
@@ -160,9 +180,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    taurus.style.display = 'block';
+    /* taurus.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -173,13 +193,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   peter.addEventListener('click', function() {
     changeText(gemini);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(peter);
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -190,9 +210,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    gemini.style.display = 'block';
+    /* gemini.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -203,13 +223,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   mirror.addEventListener('click', function(){
     changeText(cancer);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(mirror);
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -221,8 +241,8 @@ const up = document.getElementById('up')
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
     up.style.opacity = '0.4';
-
-    cancer.style.display = 'block';
+ */
+    /* cancer.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -233,13 +253,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   mal.addEventListener('click', function(){
     changeText(leo);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(mal);
+   /*  ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -251,8 +271,8 @@ const up = document.getElementById('up')
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
     up.style.opacity = '0.4';
-
-    leo.style.display = 'block';
+ */
+    /* leo.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -263,13 +283,13 @@ const up = document.getElementById('up')
     libra.style.display = 'none';
     scorpio.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
   toy.addEventListener('click', function(){
       changeText(virgo);
-
-      ariel.style.opacity = '0.4';
+      changeOpacity(toy);
+      /* ariel.style.opacity = '0.4';
       incred.style.opacity = '0.4';
       mike.style.opacity = '0.4';
       apple.style.opacity = '0.4';
@@ -280,9 +300,9 @@ const up = document.getElementById('up')
       tiana.style.opacity = '0.4';
       belle.style.opacity = '0.4';
       tangled.style.opacity = '0.4';
-      up.style.opacity = '0.4';  
+      up.style.opacity = '0.4';  */ 
 
-      virgo.style.display = 'block';
+      /* virgo.style.display = 'block';
       aquarius.style.display = "none";
       pisces.style.display = 'none';
       aries.style.display = 'none';
@@ -293,13 +313,13 @@ const up = document.getElementById('up')
       libra.style.display = 'none';
       scorpio.style.display = 'none';
       sagittarius.style.display = 'none';
-      capricorn.style.display = 'none';
+      capricorn.style.display = 'none'; */
   });
 
   tiana.addEventListener('click', function(){
       changeText(libra);
-
-      ariel.style.opacity = '0.4';
+      changeOpacity(tiana);
+      /* ariel.style.opacity = '0.4';
       incred.style.opacity = '0.4';
       mike.style.opacity = '0.4';
       apple.style.opacity = '0.4';
@@ -310,9 +330,9 @@ const up = document.getElementById('up')
       tiana.style.opacity = '1';
       belle.style.opacity = '0.4';
       tangled.style.opacity = '0.4';
-      up.style.opacity = '0.4';  
+      up.style.opacity = '0.4';  */ 
 
-      libra.style.display = 'block';
+      /* libra.style.display = 'block';
       aquarius.style.display = "none";
       pisces.style.display = 'none';
       aries.style.display = 'none';
@@ -323,13 +343,13 @@ const up = document.getElementById('up')
       virgo.style.display = 'none';
       scorpio.style.display = 'none';
       sagittarius.style.display = 'none';
-      capricorn.style.display = 'none';
+      capricorn.style.display = 'none'; */
   });
 
   belle.addEventListener('click', function(){
     changeText(scorpio);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(belle);
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -340,9 +360,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '1';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    scorpio.style.display = 'block';
+    /* scorpio.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -353,14 +373,14 @@ const up = document.getElementById('up')
     virgo.style.display = 'none';
     libra.style.display = 'none';
     sagittarius.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
   });
 
 
   tangled.addEventListener('click', function() {
     changeText(sagittarius);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(tangled);
+   /*  ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -371,9 +391,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '1';
-    up.style.opacity = '0.4';
+    up.style.opacity = '0.4'; */
 
-    sagittarius.style.display = 'block';
+    /* sagittarius.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -384,14 +404,14 @@ const up = document.getElementById('up')
     virgo.style.display = 'none';
     libra.style.display = 'none';
     scorpio.style.display = 'none';
-    capricorn.style.display = 'none';
+    capricorn.style.display = 'none'; */
 
   });
 
   up.addEventListener('click', function(){
     changeText(capricorn);
-
-    ariel.style.opacity = '0.4';
+    changeOpacity(up);
+    /* ariel.style.opacity = '0.4';
     incred.style.opacity = '0.4';
     mike.style.opacity = '0.4';
     apple.style.opacity = '0.4';
@@ -402,9 +422,9 @@ const up = document.getElementById('up')
     tiana.style.opacity = '0.4';
     belle.style.opacity = '0.4';
     tangled.style.opacity = '0.4';
-    up.style.opacity = '1';
+    up.style.opacity = '1'; */
 
-    capricorn.style.display = 'block';
+    /* capricorn.style.display = 'block';
     aquarius.style.display = "none";
     pisces.style.display = 'none';
     aries.style.display = 'none';
@@ -415,10 +435,10 @@ const up = document.getElementById('up')
     virgo.style.display = 'none';
     libra.style.display = 'none';
     scorpio.style.display = 'none';
-    sagittarius.style.display = 'none';
+    sagittarius.style.display = 'none'; */
   });
 
-
+  
 
 
   const soundButtons = document.querySelectorAll('.soundtrack');
@@ -428,12 +448,11 @@ const up = document.getElementById('up')
   soundButtons.forEach(button => {
       const sound = button.dataset.sound;
       button.addEventListener('click', () => {
-          speakers.src = `audio/${sound}.mp3`;
+          speakers.src = `../audio/${sound}.mp3`;
           speakers.play();
-      })
-  
-  })
 
+      })
+  })
 
 
 
@@ -452,6 +471,7 @@ function clickRestart(){
 
 function clickMonth() {
     // click the button to select the desired month 
+
 }
 
 function clickDay () {
@@ -472,38 +492,106 @@ function clickYear() {
 
 
 
+let whichMonth = document.getElementById('choosemonth').value//select element for month
+let whichDayOfMonth = document.getElementById('chooseday').value//select element for day
+
+const submit = document.getElementById('month');//form
 
 //IF STATEMENT FOR THE LOOP!!!   IF STATEMENT FOR THE LOOP!!!
 
 
 
-
+submit.addEventListener('submit', function(){ //calls this function when submitting the form
 if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
   AstroSign = "capricorn";
+  changeText(capricorn);
+  changeOpacity(up);
 } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
   AstroSign = "sagittarius";
+  changeText(sagittarius);
+  changeOpacity(tangled);
 } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
   AstroSign = "scorpio";
+  changeText(scorpio);
+  changeOpacity(belle);
 } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
   AstroSign = "libra";
+  changeText(libra);
+  changeOpacity(tiana);
 } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
   AstroSign = "virgo";
+  changeText(virgo);
+  changeOpacity(toy);
 } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
   AstroSign = "leo";
+  changeText(leo);
+  changeOpacity(mal);
 } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
   AstroSign = "cancer";
+  changeText(cancer);
+  changeOpacity(mirror);
 } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
   AstroSign = "gemini";
+  changeText(gemini);
+  changeOpacity(peter);
 } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
   AstroSign = "taurus";
+  changeText(taurus);
+  changeOpacity(apple);
 } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
   AstroSign = "aries";
+  changeText(aries);
+  changeOpacity(mike);
 } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
   AstroSign = "pisces";
+  changeText(pisces);
+  changeOpacity(incred);
 } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
   AstroSign = "aquarius";
+  changeText(aquarius);
+  changeOpacity(ariel);
 }
+});
 
+/* submit.addEventListener("submit", function(){
+  if (AstroSign == "capricorn") {
+    changeText(capricorn);
+    changeOpacity(up);
+  } else if (AstroSign == "sagittarius") {
+    changeText(sagittarius);
+    changeOpacity(tangled);
+  } else if (AstroSign == "scorpio") {
+    changeText(scorpio);
+    changeOpacity(belle);
+  } else if (AstroSign == "libra") {
+    changeText(libra);
+    changeOpacity(tiana);
+  } else if (AstroSign == "virgo") {
+    changeText(virgo);
+    changeOpacity(toy);
+  } else if (AstroSign == "leo") {
+    changeText(leo);
+    changeOpacity(mal);
+  } else if (AstroSign == "cancer") {
+    changeText(cancer);
+    changeOpacity(mirror);
+  } else if (AstroSign == "gemini") {
+    changeText(gemini);
+    changeOpacity(peter);
+  } else if (AstroSign == "taurus") {
+    changeText(taurus);
+    changeOpacity(apple);
+  } else if (AstroSign == "aries") {
+    changeText(aries);
+    changeOpacity(mike);
+  } else if (AstroSign == "pisces") {
+    changeText(pisces);
+    changeOpacity(incred);
+  } else if (AstroSign == "aquarius") {
+    changeText(aquarius);
+    changeOpacity(ariel);
+  }
+}); */
 
 
 
